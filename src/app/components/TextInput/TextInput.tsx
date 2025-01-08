ee07e27c32389b13d2fe726f3935a24585a7db1b
Daniel Chyliński <danielosd68@gmail.com>
2025-01-08 14:20:09 +0100

    formik integration

--- src/app/components/TextInput/TextInput.tsx
+++ src/app/components/TextInput/TextInput.tsx
@@ -1,12 +1,19 @@
 import {HTMLInputTypeAttribute, ReactNode} from "react";
+import {ErrorMessage, Field} from "formik";
 
-const TextInput = (props: {fieldset: string | ReactNode, name: string, type?: HTMLInputTypeAttribute, value?: string, onChange}) => {
+const TextInput = (props: {
+    fieldset: string | ReactNode,
+    name: string,
+    type?: HTMLInputTypeAttribute,
+    value?: string
+}) => {
     return (
         <>
             <p className={'mb-3'}>{props.fieldset}</p>
-            <input type={props.type} name={props.name} className={'outline-none w-full h-[45px] p-5 text-[16px] rounded-lg border-[1px] border-[--foreground]'} onChange={props.onChange}/>
+            <Field name={props.name} className={'outline-none w-full h-[45px] p-5 text-[16px] rounded-lg border-[1px] border-[--foreground]'}/>
+            <ErrorMessage name={props.name} render={(errorMessage) => (<p className={'text-red-500'}>{errorMessage}</p>)}/>
         </>
-    )
+)
 }
 
 export default TextInput;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,55 +2,26 @@
 import {useState} from "react";
 import TextInput from "@/app/components/TextInput/TextInput";
 import {bool, object, string} from "yup";
+import {ErrorMessage, Field, Form, Formik, FormikHelpers} from "formik";
 
 export default function Home() {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [error, setError] = useState(false);
+    const [errMessage, setErrMessage] = useState("");
+    const [loading, setLoading] = useState(false);
 
-    const [contactData, setContactData] = useState({
-        name: "",
-        surname: "",
-        email: "",
-        message: ""
-    });
 
-    const contactSchema = object({
+    const contactSchema = object().shape({
         name: string().required("Pole wymagane"),
         surname: string().required("Pole wymagane"),
         email: string().email("Wprowadź poprawną formę adresu email").required("Pole wymagane"),
         queryType: bool().oneOf([true]),
         message: string().required("Pole wymagane"),
-        agreement: bool().oneOf([true]).required("Zgoda wymagana")
+        agreement: bool().oneOf([true], "Zgoda wymagana").required()
     });
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        contactSchema.validate(contactData).then((validatedObject) => {
-            console.log(validatedObject);
-        }).catch((reason) => {
-            console.warn(reason);
-        });
-    }
-
-    const handleChange = (event) => {
-        console.log(event);
-        setContactData((prevState) => {
-            if(event.target.type !== "checkbox" && event.target.type !== "radio"){
-                return {
-                    ...prevState,
-                    [event.target.name] : event.target.value
-                }
-            }
-
-            else{
-                if(event.target.type === "checkbox"){
-                    return {
-                        ...prevState,
-                        [event.target.name] : event.target.checked
-                    }
-                }
-            }
-        });
+    const handleSubmit = (values, formikHelpers: FormikHelpers<any>) => {
+        setLoading(true);
     }
 
     return (
@@ -60,38 +31,50 @@ export default function Home() {
                     <h1 className={'text-3xl font-bold'}>Wyślij wiadomość</h1>
                 </header>
                 <section>
-                    <form>
-                        <div className={'grid grid-cols-2 gap-5 mt-5 items-stretch'}>
-                            <div>
-                                <TextInput fieldset={'Imię'} name={'name'} value={contactData.name} onChange={handleChange}/>
-                            </div>
-                            <div>
-                                <TextInput fieldset={'Nazwisko'} name={'surname'} value={contactData.surname} onChange={handleChange}/>
-                            </div>
+                    <Formik initialValues={{
+                        name: "",
+                        surname: "",
+                        email: "",
+                        message: "",
+                        agreement: false
+                    }} onSubmit={handleSubmit} enableReinitialize={true} validationSchema={contactSchema}>
+                        {({errors, touched}) => (
+                            <Form>
+                                <div className={'grid grid-cols-2 gap-5 mt-5 items-stretch'}>
+                                    <div>
+                                        <TextInput fieldset={'Imię'} name={'name'}/>
+                                    </div>
+                                    <div>
+                                        <TextInput fieldset={'Nazwisko'} name={'surname'}/>
+                                    </div>
 
-                            <div className={'col-span-2'}>
-                                <TextInput fieldset={'Email'} name={'email'} type={'email'} value={contactData.email} onChange={handleChange}/>
-                            </div>
+                                    <div className={'col-span-2'}>
+                                        <TextInput fieldset={'Email'} name={'email'} type={'email'}/>
+                                    </div>
 
-                            <div className={'col-span-2'}>
-                            <p className={'mb-3'}>Wiadomość</p>
-                                <textarea value={contactData.message} name="message" className={'outline-none w-full border-[1px] border-[--foreground] rounded-lg p-5'} rows={5} onChange={handleChange}></textarea>
-                            </div>
+                                    <div className={'col-span-2'}>
+                                        <p className={'mb-3'}>Wiadomość</p>
+                                        <Field as={"textarea"} name={'message'} className={'outline-none w-full border-[1px] border-[--foreground] rounded-lg p-5'} rows={5}/>
+                                        <ErrorMessage name={'message'} render={(errorMessage) => (<p className={'text-red-500'}>{errorMessage}</p>)}/>
+                                    </div>
 
-                            <div className={'col-span-2'}>
-                                <input type="checkbox" name={'agreement'} className={'mr-5'} id={'agreement'} onChange={handleChange}/>
-                                <label htmlFor="agreement">Wyrażam zgodę na otrzymanie informacji zwrotnej z pomocą.</label>
-                            </div>
+                                    <div className={'col-span-2'}>
+                                        <Field type={"checkbox"} name={'agreement'} className={'mr-5'} id={'agreement'}/>
+                                        <label htmlFor="agreement">Wyrażam zgodę na otrzymanie informacji zwrotnej z pomocą.</label>
+                                        <ErrorMessage name={'agreement'} render={(errorMessage) => (<p className={'text-red-500'}>{errorMessage}</p>)}/>
+                                    </div>
 
-                            <div className={'col-span-2 mt-10'}>
-                                <button
-                                    onClick={(e) => {handleSubmit(e)}}
-                                    className={'bg-[--main-green] text-white border-0 w-full h-[50px] rounded-lg'}>Wyślij
-                                </button>
-                                {error && (<p className={'text-[--error] mt-3'}>Wystąpił błąd w wysyłce wiadomości. Spróbuj ponownie.</p>)}
-                            </div>
-                        </div>
-                    </form>
+                                    <div className={'col-span-2 mt-10'}>
+                                        <button
+                                            disabled={loading}
+                                            className={'bg-[--main-green] text-white border-0 w-full h-[50px] rounded-lg ' + (loading ? 'opacity-30' : '')}>Wyślij
+                                        </button>
+                                        {error && (<p className={'text-[--error] mt-3'}>Wystąpił błąd w wysyłce formularza - {errMessage}</p>)}
+                                    </div>
+                                </div>
+                            </Form>
+                        )}
+                    </Formik>
                 </section>
             </div>
         </main>
